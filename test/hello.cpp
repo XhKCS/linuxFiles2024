f488b7b067b9094e26ff56681b2fbbbcea9d03cf
xh <126169917+XhKCS@users.noreply.github.com>
2024-12-12 11:33:52 +0800

    Add files via upload

---
+++ test/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello,world!!\n";
+  string s1 = "Linux";
+  cout << "Hello, " << s1 << "\n";
+  cout << "Hey\n";
+  return 0;
+}
+
